483ac5f0ecbb570e2dbeae33a0ecf09246c06e1c
Fazil Salam <111173593+Fazilsalam@users.noreply.github.com>
2024-05-30 17:24:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,31 +13,4 @@ const App = () => {
 
 export default App;
 
-// import React, { useContext, useState } from "react";
-// import { Context } from "./context/Context";
 
-// const App = () => {
-//   const { response, onSent } = useContext(Context);
-//   const [prompt, setPrompt] = useState("");
-
-//   const handleSend = () => {
-//     onSent(prompt);
-//   };
-
-//   return (
-//     <div>
-//       <input
-//         type="text"
-//         value={prompt}
-//         onChange={(e) => setPrompt(e.target.value)}
-//       />
-//       <button onClick={handleSend}>Send</button>
-//       <div>
-//         <h2>Response:</h2>
-//         <p>{response}</p>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
